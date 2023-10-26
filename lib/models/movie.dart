7c9cf53cbb328756a903f03aa3f956a027a70a9f
Viktor Naustinnyi <92892873+Fizzzle@users.noreply.github.com>
2023-10-26 19:53:12 +0300

    movie config add

---
+++ lib/models/movie.dart
@@ -0,0 +1,48 @@
+// Packages
+import 'package:get_it/get_it.dart';
+
+// Models
+import '../models/app_config.dart';
+
+class Movie {
+  final String name;
+  final String language;
+  final bool isAdult;
+  final String description;
+  final String posterPath;
+  final String backdropPath;
+  final num rating;
+  final String realeaseDate;
+  final int id;
+
+  Movie({
+    required this.name,
+    required this.language,
+    required this.isAdult,
+    required this.description,
+    required this.posterPath,
+    required this.backdropPath,
+    required this.rating,
+    required this.realeaseDate,
+    required this.id,
+  });
+
+  factory Movie.fromJson(Map<String, dynamic> _json) {
+    return Movie(
+      name: _json['title'],
+      language: _json['original_language'],
+      isAdult: _json['adult'],
+      description: _json['overview'],
+      posterPath: _json['poster_path'],
+      backdropPath: _json['backdrop_path'],
+      rating: _json['vote_average'],
+      realeaseDate: _json['release_date'],
+      id: _json['id'],
+    );
+  }
+
+  String posterURL() {
+    final AppConfig _appConfig = GetIt.instance.get<AppConfig>();
+    return '${_appConfig.BASE_IMAGE_API}${this.posterPath}';
+  }
+}
